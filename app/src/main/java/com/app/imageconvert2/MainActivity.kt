096273f1027990952d997f5af69f6e37069d2a9f
Dhirender <dhirender.kumar@rabblesoft.com>
2022-04-21 15:17:36 +0530

    design xml and camera and gallary and convert to phone pdf

--- app/src/main/java/com/app/imageconvert2/MainActivity.kt
+++ app/src/main/java/com/app/imageconvert2/MainActivity.kt
@@ -1,77 +1,183 @@
 package com.app.imageconvert2
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.pdf.PdfDocument
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.FileOutputStream
 
+
 class MainActivity : AppCompatActivity() {
+    var bitmap: Bitmap? = null
+
+    val permissionOfarray = arrayOf(
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.CAMERA
+    )
+    val REQUEST_CODE = 200
+    var isCamera = false
+    var converterimage = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        camera.setOnClickListener {
+//            if (askPermission()) {
+//                isCamera = false
+//                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//                resultLauncher.launch(intent)
+//            }
+//
+//        }
+//        gallary.setOnClickListener {
+//            if (askPermission()) {
+//                isCamera = true
+//                val intent =
+//                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//                resultLauncher.launch(intent)
+//            }
+//        }
+        camera.setOnClickListener {
+            isCamera = true
+            askPermission()
+        }
+        gallary.setOnClickListener {
+            isCamera = false
+            askPermission()
+        }
+        convert.setOnClickListener {
+            if (bitmap != null) {
 
-        button.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(intent,120)
+                val pdfDocument = PdfDocument()
+                val pageinfo = PdfDocument.PageInfo.Builder(960, 1280, 1).create()
+                val page = pdfDocument.startPage(pageinfo)
+                val canvas = page.canvas
+                val paint = Paint()
+//                  paint.set(Color.parseColor("#FFFFFF"))
+                canvas.drawPaint(paint)
+                bitmap = Bitmap.createScaledBitmap(bitmap!!, 960, 1280, true)
+                paint.setColor(Color.BLUE)
+                canvas.drawBitmap(bitmap!!, 0f, 0f, null)
+                pdfDocument.finishPage(page)
+
+
+                val root = File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),
+                    "My_directory"
+                )
+                if (!root.exists()) {
+                    root.mkdirs()
+                }
+                val file = File(root, "picture.pdf")
+                try {
+                    val fileOutputStream = FileOutputStream(file)
+                    pdfDocument.writeTo(fileOutputStream)
+                    Toast.makeText(this, "Image Convert Successfully in PDF | internal mermery/Documents/my_directory", Toast.LENGTH_LONG)
+                        .show()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    Toast.makeText(this, "Something Wrong, Please Try agin!!", Toast.LENGTH_LONG)
+                        .show()
+                }
+                pdfDocument.close()
+            } else {
+                Toast.makeText(this, "Please Select the picture First", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+    private fun askPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) + ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            + ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, permissionOfarray, REQUEST_CODE)
+        } else {
+            if (isCamera) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                resultLauncher.launch(intent)
+            } else {
+                val intent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                resultLauncher.launch(intent)
+            }
+
+
         }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode ==120 && resultCode == RESULT_OK&& data!=null){
-            val selectedImageUrl = data.data
-            val filepath = arrayOf(MediaStore.Images.Media.DATA)
-
-            val cursor = contentResolver.query(selectedImageUrl!!,filepath,null,null,null)
-            cursor!!.moveToFirst()
-            val columIndex = cursor!!.getColumnIndex(filepath[0].toString())
-            val myPath = cursor!!.getString(columIndex)
-            cursor.close()
-            var bitmap = BitmapFactory.decodeFile(myPath)
-            image.setImageBitmap(bitmap)
-
-
-            val pdfDocument = PdfDocument()
-            val pageinfo = PdfDocument.PageInfo.Builder(960, 1280, 1).create()
-            val page = pdfDocument.startPage(pageinfo)
-            val canvas = page.canvas
-            val paint = Paint()
-//            paint.set(Color.parseColor("#FFFFFF"))
-
-            canvas.drawPaint(paint)
-
-            bitmap = Bitmap.createScaledBitmap(bitmap, bitmap.width,bitmap.height,true)
-            paint.setColor(Color.BLUE)
-            canvas.drawBitmap(bitmap,0f,0f,null)
-            pdfDocument.finishPage(page)
-
-
-            //save bitmap image
-            val root = File(Environment.getExternalStorageDirectory(),"PDF Folder 12")
-            if (!root.exists()){
-                root.mkdirs()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE) {
+//            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            if (isCamera) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                resultLauncher.launch(intent)
+            } else {
+                val intent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                resultLauncher.launch(intent)
             }
-            val file = File(root,"picture.pdf")
-            try {
-                val fileOutputStream = FileOutputStream(file)
-                pdfDocument.writeTo(fileOutputStream)
-            }catch (e:Exception){
-                e.printStackTrace()
-            }
-            pdfDocument.close()
-
-
 
         }
     }
+
+
+    val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            Log.e("resultLauncher", result.toString())
+            when (result.resultCode) {
+                Activity.RESULT_OK -> {
+
+                    if (isCamera) {
+                        bitmap = result.data?.getExtras()?.get("data") as Bitmap
+                        // Set the image in imageview for display
+                        // Set the image in imageview for display
+                        image.setImageBitmap(bitmap)
+                    } else {
+                        val selectedImageUrl = result.data?.data
+                        val filepath = arrayOf(MediaStore.Images.Media.DATA)
+                        val cursor =
+                            contentResolver.query(selectedImageUrl!!, filepath, null, null, null)
+                        cursor!!.moveToFirst()
+                        val columIndex = cursor!!.getColumnIndex(filepath[0].toString())
+                        val myPath = cursor!!.getString(columIndex)
+                        cursor.close()
+                        bitmap = BitmapFactory.decodeFile(myPath)
+                        image.setImageBitmap(bitmap)
+                    }
+
+
+                }
+            }
+        }
 }
